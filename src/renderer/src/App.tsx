9877a0666e0e0cd504c581532b9063ff696303c2
Raffaele <raffaeletrivillin@archeido.com>
2025-04-03 18:10:12 +0200

    feat: subscribe to stats event in renderer

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -1,13 +1,26 @@
 import { Button, Space } from 'antd'
+import { useEffect } from 'react';
 
 function App(): JSX.Element {
   const ipcHandle = (): void => window.electron.ipcRenderer.send('ping')
 
+  // calling the method in the useEffect to avoid subscribing multiple times
+  useEffect(() => {
+    // @ts-ignore
+    window.api.subscribeStats((stats) => console.log(stats));
+  }, []);
+
+  const printStaticStats = (): void => {
+    // @ts-ignore
+    const stats = window.api.getStaticData()
+    console.log(stats)
+  };
+  
   return (
     <>
       <Space direction="vertical" size={16}>
         <Button onClick={ipcHandle}>Click me</Button>
-        <Button type="primary">Click me 2</Button>
+        <Button onClick={printStaticStats} type="primary">Click me 2</Button>
       </Space>
     </>
   )
