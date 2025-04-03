a133890b4cf5d9fcfdf41a62468decdd42648c86
Raffaele <raffaeletrivillin@archeido.com>
2025-04-03 17:58:23 +0200

    feat: add resourceManager

---
+++ src/main/app/resourceManager.ts
@@ -0,0 +1,53 @@
+import { RESOURCE_POLLING_RATE } from "../consts";
+import osUtils from "os-utils";
+import fs from "fs";
+import os from "os";
+import { BrowserWindow } from "electron";
+
+export function pollingResources(window: BrowserWindow) {
+  setInterval(async () => {
+    const cpu = await getCpuUsage();
+    const ram = await getRamUsage();
+    const disk = await getDiskUsage();
+    window.webContents.send("os-stats", { cpu, ram, disk });
+  }, RESOURCE_POLLING_RATE);
+}
+
+export function getStaticData() {
+  const totalMem = Math.floor(osUtils.totalmem() / 1024);
+  const totalDisk = getDiskUsage().total;
+  const cpu = os.cpus()[0];
+  return {
+    totalMem,
+    totalDisk,
+    cpu: { model: cpu.model, speed: cpu.speed }
+  };
+}
+
+function getCpuUsage() {
+  return new Promise((resolve) => osUtils.cpuUsage(resolve));
+}
+
+function getRamUsage() {
+  const totalMem = osUtils.totalmem();
+  const usedMem = totalMem - osUtils.freemem();
+
+  return {
+    value: usedMem,
+    percentage: usedMem / totalMem
+  };
+}
+
+function getDiskUsage() {
+  const stats = fs.statfsSync(process.platform === "win32" ? "C:\\" : "/");
+  const total = stats.blocks * stats.bsize;
+  const free = stats.bfree * stats.bsize;
+  const used = total - free;
+
+  return {
+    total: total / 1_000_000_000,
+    used: used / 1_000_000_000,
+    free: free / 1_000_000_000,
+    usedPercentage: (used / total) * 100
+  };
+}
