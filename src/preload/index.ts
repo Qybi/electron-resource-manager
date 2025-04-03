b9ecaf96aedb8a79877cc5ff0da2ab278aef89fc
Raffaele <raffaeletrivillin@archeido.com>
2025-04-03 18:08:12 +0200

    feat: expose custom main process apis to preload script

--- src/preload/index.ts
+++ src/preload/index.ts
@@ -1,22 +1,32 @@
-import { contextBridge } from 'electron'
-import { electronAPI } from '@electron-toolkit/preload'
+import { contextBridge } from "electron";
+import { electronAPI } from "@electron-toolkit/preload";
+import { getStaticData } from "../main/app/resourceManager";
+import { Api } from "./index.d";
 
 // Custom APIs for renderer
-const api = {}
+// define here functions that will be exposed to the renderer (frontend)
+const api: Api = {
+  getStaticData: () => getStaticData(),
+  subscribeStats: (callback: (stats: any) => void) => {
+    electronAPI.ipcRenderer.on("os-stats", (_ /*event*/, data) => {
+      callback(data);
+    });
+  }
+};
 
 // Use `contextBridge` APIs to expose Electron APIs to
 // renderer only if context isolation is enabled, otherwise
 // just add to the DOM global.
 if (process.contextIsolated) {
   try {
-    contextBridge.exposeInMainWorld('electron', electronAPI)
-    contextBridge.exposeInMainWorld('api', api)
+    contextBridge.exposeInMainWorld("electron", electronAPI);
+    contextBridge.exposeInMainWorld("api", api);
   } catch (error) {
-    console.error(error)
+    console.error(error);
   }
 } else {
   // @ts-ignore (define in dts)
-  window.electron = electronAPI
+  window.electron = electronAPI;
   // @ts-ignore (define in dts)
-  window.api = api
+  window.api = api;
 }
