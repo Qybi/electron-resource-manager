f8b82e1286a1a9b0dd99b3cb4230507320d401f1
Raffaele <raffaeletrivillin@archeido.com>
2025-04-04 17:28:13 +0200

    feat: use ipc wrappers and set return types for each function

--- src/main/app/resourceManager.ts
+++ src/main/app/resourceManager.ts
@@ -3,15 +3,17 @@ import osUtils from "os-utils";
 import fs from "fs";
 import os from "os";
 import { BrowserWindow } from "electron";
+import { ipcWebContentsSend } from "../utils/utils";
+import { Stats } from "../../types/stats";
 
 export function pollingResources(window: BrowserWindow) {
   setInterval(async () => {
     const cpu = await getCpuUsage();
     const ram = await getRamUsage();
     const disk = await getDiskUsage();
-    // this creates an event called 'os-stats' and sends the data on it.
+    // this creates an event called 'stats' and sends the data on it.
     // the frontend CAN subscribe to this event in the preload/index.ts file that will expose an api for the renderer (frontend) process
-    window.webContents.send("os-stats", { cpu, ram, disk });
+    ipcWebContentsSend("stats", window.webContents, { cpu, ram, disk });
   }, RESOURCE_POLLING_RATE);
 }
 
@@ -26,11 +28,11 @@ export function getStaticData() {
   };
 }
 
-function getCpuUsage() {
-  return new Promise((resolve) => osUtils.cpuUsage(resolve));
+function getCpuUsage(): Promise<number> {
+  return new Promise<number>((resolve) => osUtils.cpuUsage(resolve));
 }
 
-function getRamUsage() {
+function getRamUsage(): Stats["ram"] {
   const totalMem = osUtils.totalmem();
   const usedMem = totalMem - osUtils.freemem();
 
@@ -40,7 +42,7 @@ function getRamUsage() {
   };
 }
 
-function getDiskUsage() {
+function getDiskUsage(): Stats["disk"] {
   const stats = fs.statfsSync(process.platform === "win32" ? "C:\\" : "/");
   const total = stats.blocks * stats.bsize;
   const free = stats.bfree * stats.bsize;
