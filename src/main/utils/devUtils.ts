add94a0db910394277ac5ae67989145e47c66a3e
Raffaele <raffaeletrivillin@archeido.com>
2025-04-04 17:59:17 +0200

    feat: add helper for ui path

--- src/main/utils/devUtils.ts
+++ src/main/utils/devUtils.ts
@@ -1,3 +1,9 @@
+import { join } from "path";
+
 export function isDev(): boolean {
-  return process.env.NODE_ENV === 'development'
+  return process.env.NODE_ENV === "development";
+}
+
+export function getUiPath(): string {
+  return join(__dirname, "../renderer/index.html");
 }
